c86007adb89d13e5dd2606db13bc506a1132ae32
Tymofii Sukhachov <tymofii.sukhachov@tipico.com>
2021-03-14 16:00:39 +0100

    added functional style method

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
-    public static final String DOT = ".";
-    public static final int X_LENGTH = 6;
-    public static final String MISS = "O";
-    public static final String MATCH = "X";
-    public static final String[] Y_CHARS = {"A", "B", "C", "D", "E", "F"};
+    private static final int X_LENGTH = 6;
+    private static final String DOT = ".";
+    private static final String MISS = "O";
+    private static final String MATCH = "X";
+    private static final String[] Y_CHARS = {"A", "B", "C", "D", "E", "F"};
 
     public static void main(String[] args) {
 
@@ -22,36 +25,79 @@ public class Main {
         final Point miss = new Point(1, "B"); // failing attempt
         final Point missAtLastCharacter = new Point(5, "D"); // failing attempt
 
+        final long start1 = System.currentTimeMillis();
         System.out.println("===== A hit =======");
         play(hit, ships);
-        System.out.println("===== A hit at last char =======");
-        play(hitAtLastChar, ships);
+        final long end1 = System.currentTimeMillis();
+        System.out.println("Took: " + (end1 - start1));
+
+        final long start2 = System.currentTimeMillis();
+        System.out.println("===== A hit (functional style) =======");
+        playFunctionalStyle(hit, ships);
+        final long end2 = System.currentTimeMillis();
+        System.out.println("Took: " + (end2 - start2));
+
         System.out.println("===== A miss =======");
         play(miss, ships);
+        System.out.println("===== A miss (functional style) =======");
+        playFunctionalStyle(miss, ships);
+        System.out.println("===== A hit at last char =======");
+        play(hitAtLastChar, ships);
         System.out.println("===== A miss at last char =======");
         play(missAtLastCharacter, ships);
-
     }
 
-    private static void play(Point hitAttempt, List<Point> ships) {
+    private static void playFunctionalStyle(Point hitAttempt, List<Point> playerShips) {
+        final HashMap<Point, String> attemptStore = new HashMap<>();
+
+        streamOfAllPoints()
+            .parallel()
+            .filter(hitAttempt::equals)
+            .findAny()
+            .ifPresent(p -> attemptStore.put(p, playerShips.contains(p) ? MATCH : MISS));
+
+        printMatrix(attemptStore);
+    }
+
+    private static void printMatrix(HashMap<Point, String> attemptStore) {
+        streamOfAllPoints()
+            .sorted(Comparator
+                .comparing(Point::getY)
+                .thenComparingInt(Point::getX))
+            .forEach(point -> {
+                    System.out.print(attemptStore.getOrDefault(point, DOT));
+                    newLine(point);
+                }
+            );
+    }
+
+    private static void newLine(Point point) {
+        if (point.getX() == X_LENGTH - 1) {
+            System.out.println();
+        }
+    }
+
+    private static void play(Point hitAttempt, List<Point> playerShips) {
         for (String yChar : Y_CHARS) {
             for (int i = 0; i < X_LENGTH; i++) {
-                final boolean isLastDot = i == X_LENGTH - 1;
                 final Point currentPoint = new Point(i, yChar);
                 if (hitAttempt.equals(currentPoint)) {
-                    printNewOrSameLine(isLastDot, ships.contains(hitAttempt) ? MATCH : MISS);
+                    if (playerShips.contains(hitAttempt)) {
+                        System.out.print(MATCH);
+                    } else {
+                        System.out.print(MISS);
+                    }
                 } else {
-                    printNewOrSameLine(isLastDot, DOT);
+                    System.out.print(DOT);
                 }
             }
+            System.out.println(); // new line
         }
     }
 
-    private static void printNewOrSameLine(boolean isLastDot, String character) {
-        if (isLastDot) {
-            System.out.println(character);
-        } else {
-            System.out.print(character);
-        }
+    private static Stream<Point> streamOfAllPoints() {
+        return Stream.of(Y_CHARS).flatMap(y -> Stream.iterate(0, n -> n + 1)
+            .limit(X_LENGTH).map(x -> new Point(x, y)));
     }
+
 }
